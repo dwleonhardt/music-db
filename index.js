489e32ab8101879c04ab7aeae74da53153ff249f
dwleonhardt <dwleonhardt@gmail.com>
2017-06-01 15:58:33 -0600

    added .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log('work');
 });
 app.use(express.static('public'));
